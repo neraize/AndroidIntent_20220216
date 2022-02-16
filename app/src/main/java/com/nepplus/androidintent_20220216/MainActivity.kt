c7bd1d980624b640aa9fa6f4d92dce1b6e60d368
seojungmin <neraize@gmail.com>
2022-02-16 11:47:54 +0900

    플레이스토어로 바로링크

--- app/src/main/java/com/nepplus/androidintent_20220216/MainActivity.kt
+++ app/src/main/java/com/nepplus/androidintent_20220216/MainActivity.kt
@@ -55,5 +55,12 @@ class MainActivity : AppCompatActivity() {
             val myIntent = Intent(Intent.ACTION_VIEW, myUri)
             startActivity(myIntent)
         }
+
+
+        btnKakaoTalkPlayStoreLink.setOnClickListener {
+            val myUri = Uri.parse("market://details?id=com.kakao.talk")
+            val myIntent = Intent(Intent.ACTION_VIEW, myUri)
+            startActivity(myIntent)
+        }
     }
 }
